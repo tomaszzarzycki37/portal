bbde0f93822efbd8c7b784cce8e39eb90e25284a
Zarzycki <tomasz.zarzycki@teleste.com>
2026-05-12 07:34:46 +0200

    Change footer brand text to Auta Chin

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -32,7 +32,7 @@ export default function Footer() {
       </div>
 
       <div className="footer-bottom">
-        <p>&copy; {currentYear} AutaChin. {t.footer.rights}</p>
+        <p>&copy; {currentYear} Auta Chin. {t.footer.rights}</p>
       </div>
     </footer>
   )
